f2e9134cf2ae5af457d47da7861e2dd813b37a79
Carlos Olivo <carlosengel011@gmail.com>
2023-06-20 17:44:38 -0400

    readme added

--- src/common/utils/hash.ts
+++
@@ -1,6 +0,0 @@
-export async function hash(text: string): Promise<ArrayBuffer> {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(text)
-  const hash = await crypto.subtle.digest('SHA-256', data)
-  return hash
-}
